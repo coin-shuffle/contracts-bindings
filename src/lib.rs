f50892705bfe2d801ecf5ebce5486eea46ba5729
Velnbur <kyrylo.baybula@distributedlab.com>
2023-02-20 20:01:43 +0200

    Some structure refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,2 @@
-macro_rules! include_contract {
-    ($contract:tt) => {
-        include!(concat!(env!("OUT_DIR"), "/", $contract, ".rs"));
-    };
-}
-
-pub mod utxo {
-    include_contract!("IUTXO");
-}
+mod macros;
+pub mod utxo;
